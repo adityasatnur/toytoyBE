25dcf21022b69a367916686e7fceab7ea8a02d61
adityasatnur <adityasatnur@gmail.com>
2021-07-05 12:40:33 +0530

    adding API and some modifications

--- itemModel.js
+++ itemModel.js
@@ -10,6 +10,7 @@ const toysSchema = mongoose.Schema({
   inventory: Number,
   type: String,
   purchasable: Boolean,
+  cost: Number,
   popular: Boolean,
   ageGroup: Array,
   toySet: String,
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import Item from "./itemModel.js";
 import User from "./userModel.js";
 import cors from "cors";
 
-
 const connection_url =
   "mongodb+srv://Aditya:toytoy@cluster0.ra9uy.mongodb.net/toytoy";
 
@@ -12,12 +11,12 @@ mongoose.connect(connection_url, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 const connection = mongoose.connection;
 
-connection.once("open", function() {
+connection.once("open", function () {
   console.log("MongoDB database connection established successfully");
 });
 //app config
@@ -29,31 +28,30 @@ const port = process.env.PORT || 9000;
 
 app.use(express.json());
 
-
 //db config
 
 //api endpoints
 
 app.get("/api/get/addItem", async (req, res) => {
-	const items = await Item.find();
+  const items = await Item.find();
   res.send(items);
+});
 
-})
-
-  app.post("/api/addItem", (req, res, next) => {
+app.post("/api/addItem", (req, res, next) => {
   const item = new Item({
     _id: mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price,
-    itemDescription:req.body.description,
-     image: req.body.image,
+    itemDescription: req.body.description,
+    image: req.body.image,
     category: req.body.category,
     inventory: req.body.inventory,
     type: req.body.type,
     purchasable: req.body.purchasable,
+    cost: req.body.cost,
     popular: req.body.popular,
     ageGroup: req.body.ageGroup,
-    toySet: req.body.toySet
+    toySet: req.body.toySet,
   });
   item
     .save()
@@ -65,59 +63,71 @@ app.get("/api/get/addItem", async (req, res) => {
     });
 });
 
-app.post("/api/updateUserData", async(req, res, next) => {
-
+app.post("/api/updateUserData", async (req, res, next) => {
   const filter = { userEmail: req.body.email };
-const update = {userPhoneNumber: req.body.phoneNumber,
-                userKidName:req.body.kidName,
-                userKidSchool:req.body.kidSchoolName,
-                userAddress:req.body.address,
-                userPincode:req.body.pinCode,
-                ReferredBy:req.body.referredBy}
+  const update = {
+    userPhoneNumber: req.body.phoneNumber,
+    userKidName: req.body.kidName,
+    userKidSchool: req.body.kidSchoolName,
+    userAddress: req.body.address,
+    userPincode: req.body.pinCode,
+    ReferredBy: req.body.referredBy,
+  };
 
-// `doc` is the document _before_ `update` was applied
-User.findOneAndUpdate(filter, update, null, function (err, docs) {
-  if (err){
-    res.status(400).send("Updating user failed");
-
-  }
-  else{
-    res.status(200).json({ user: "Profile updated successfully" });
-  }
-})
-  
+  // `doc` is the document _before_ `update` was applied
+  User.findOneAndUpdate(filter, update, null, function (err, docs) {
+    if (err) {
+      res.status(400).send("Updating user failed");
+    } else {
+      res.status(200).json({ user: "Profile updated successfully" });
+    }
+  });
 });
 
 app.post("/api/updateOrCreateUser", (req, res, next) => {
   const user = new User({
     _id: mongoose.Types.ObjectId(),
     userName: req.body.userName,
-    userEmail: req.body.userEmail
+    userEmail: req.body.userEmail,
+    userPhoneNumber: null,
+    userKidName: null,
+    userKidSchool: null,
+    userAddress: null,
+    userPincode: null,
+    userSubscriptionEndDate: null,
+    userSubscriptionStartDate: null,
+    userPlanType: "0",
+    ReferredBy: null,
   });
-  User.findOne({'userEmail': req.body.userEmail}, function(err, obj){
-    if(obj===null){
+  User.findOne({ userEmail: req.body.userEmail }, function (err, obj) {
+    if (obj === null) {
       user
         .save()
         .then(() => {
-      res.send({
-        userName: req.body.userName,
-    userEmail: req.body.userEmail
-      });
+          res.send({
+            userName: req.body.userName,
+            userEmail: req.body.userEmail,
+            userPhoneNumber: null,
+            userKidName: null,
+            userKidSchool: null,
+            userAddress: null,
+            userPincode: null,
+            userSubscriptionEndDate: null,
+            userSubscriptionStartDate: null,
+            userPlanType: "0",
+            ReferredBy: null,
+          });
 
-          res.status(200).json({ user: "user added successfully" });
-
+          //res.status(200).json({ user: "user added successfully" });
         })
         .catch((err) => {
           res.status(400).send("adding new user failed");
         });
-
+    } else {
+      res.send(obj);
     }
-    else{
-      res.send(obj)
-    }
-  })
+  });
 });
-
 
 app.get("/api", (req, res) => res.status(200).send("yo"));
 
